2e2abb44c2932effff323fb2a72f01c1a5c69487
ayoung600 <alyoung2@millersville.edu>
2022-02-17 21:36:39 -0500

    Week 4 Madlib Mad Libs Final

--- week-04-dom-manipulation/script.js
+++ week-04-dom-manipulation/script.js
@@ -8,11 +8,25 @@ console.log (message);
 document.write(message);// them to the outputs.
 function fillOutputs() {
   // Get the adjective input element
+const adjectiveTwoInput= document.getElementById('adjective-two-input');
+
+const adjectiveTwo = adjectiveTwoInput.value;
+
+const nounOneInput= document.getElementById('noun-one-input');
+
+const nounOne = nounOneInput.value;
+
   const adjectiveOneInput = document.getElementById('adjective-one-input');
   // Get the text from the input element
   const adjectiveOne = adjectiveOneInput.value;
 
+  const adjectiveTwoOutput = document.getElementById('adjective-two-output');
+
+  adjectiveTwoOutput.innerText = adjectiveTwo;
+
+const nounOneOutput= document.getElementById('noun-one-output');
   // Get the adjective output element
+nounOneOutput.innerText = nounOne;
   const adjectiveOneOutput = document.getElementById('adjective-one-output');
   // Set the text of the input element
   adjectiveOneOutput.innerText = adjectiveOne;
