5d614f5482f9553d389cb0679a99a244820d51c8
ayoung600 <alyoung2@millersville.edu>
2022-03-03 22:58:00 -0500

    For-Loops

--- week-06-for-loops/script.js
+++ week-06-for-loops/script.js
@@ -1,12 +1,17 @@
 // Replace this speeh with your own.
 // Notice the `` backticks, which let you split your string into multiple lines.
-const speech = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-    esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-    est laborum.`;
+const speech = `And last, my mom. I do not think you know what you did.
+You had my brother when you were 18 years old. Three years later, I came out.
+The odds were stacked against us. Single parent with two boys by the time you were 21 years old.
+Everybody told us we were not supposed to be here. We went from apartment to apartment by ourselves.
+One of the best memories I had was when we moved into our first apartment, no bed, no furniture and we just sat in the living room and just hugged each other.
+We thought we made it.
+
+When something good happens to you, I do not know about you guys, but I tend to look back to what brought me here.
+You wake me up in the middle of the night in the summer times, making me run up a hill, making me do pushups,
+screaming at me from the sidelines of my games at 8 or 9 years old.
+We was not supposed to be here. You made us believe. You kept us off the street. You put clothes on our backs, food on the table.
+When you did not eat, you made sure we ate. You went to sleep hungry. You sacrificed for us. You the real MVP.`;
 
 // Remove punctuation from the speech. You might have to modify this if your
 // speech contains other punctuation.
@@ -33,12 +38,77 @@ function displayLongWords() {
   }
 }
 
+
+  function displayShortWords() {
+      const shortWordsElement = document.getElementById('short-words');
+
+      for(let i = 0; i < wordsArray.length; i++){
+        const word = wordsArray[i];
+
+        console.log(word);
+        if(word.length < 4) {
+          const wordElement = document.createElement('li');
+          wordElement.innerText = word;
+          shortWordsElement.appendChild(wordElement);
+        }
+      }
+}
+
+  function displayFirstWords() {
+    const firstWordsElement = document.getElementById('first-words');
+    for(let i = string.startswith('a'); {
+
+
+      if(word = startswith('a')
+
+
+
+        const wordElement = document.createElement('li');
+        wordElement.innerText = word;
+        firstWordsElement.appendChild(wordElement);
+  }
+}
+
+  function displayThirdWords() {
+    const thirdWordsElement = document.getElementById('third-words');
+
+    for(let i = 0; i < wordsArray.length; i += 3){
+      const word = wordsArray[i];
+
+      console.log(word);
+      if(word.length ) {
+        const wordElement = document.createElement('li');
+        wordElement.innerText = word;
+        thirdWordsElement.appendChild(wordElement);
+
+      }
+    }
+}
+
+  function displayUncommonWords() {
+
+    let uncommonWordsElement = document.getElementById('uncommon-words');
+
+    for(let word of wordsArray) {
+      console.log(word);
+
+      if(word != 'the' && word != 'be' && word != 'of' && word != 'to' && word != 'in' && word != 'is' && word != 'you' && word != 'it' && word != 'was' && word != 'for') {
+        const wordElement = document.createElement('li');
+        wordElement.innerText = word;
+        uncommonWordsElement.appendChild(wordElement);
+      }
+    }
+}
 // TODO: Define your own functions here!
 
 function displaySpeechStats() {
   document.getElementById('speech').innerText = speech;
 
   displayLongWords();
+  displayShortWords();
+  displayFirstWords();
+  displayThirdWords();
+  displayUncommonWords();
 
   // TODO: Call your functions here!
 }
